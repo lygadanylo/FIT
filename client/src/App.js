546e437a778d73a6ce8860fae894cb05930329cc
Lyga Danylo <dlyg@keplercode.com>
2019-07-02 05:42:49 -0700

    create route

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,21 @@ import Login from "./components/auth/Login";
 import { connect } from "react-redux";
 import "./style/app.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { HOME_ROUTER, LOGIN_PAGE, REGISTER_PAGE } from "./common/common";
+import { LOGIN_PAGE, REGISTER_PAGE } from "./common/common";
 
 class App extends Component {
   render() {
     return (
-      <section className="App">
-        <Router basename={HOME_ROUTER}>
+      <Router basename="/">
+        <section className="App">
           <div className="app-bgn">
             <Switch>
               <Route exact path={REGISTER_PAGE} component={Register} />
               <Route exact path={LOGIN_PAGE} component={Login} />
             </Switch>
-            <Register />
           </div>
-        </Router>
-      </section>
+        </section>
+      </Router>
     );
   }
 }
