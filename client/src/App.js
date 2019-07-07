1de25e07eb6107517ff413f29b949dffc231475d
lygadanylo <31656731+lygadanylo@users.noreply.github.com>
2019-07-07 16:43:54 +0300

    style for home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,26 @@ import React, { Component } from "react";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Profile from "./components/profile/profile"
+import Navigation from "./components/landing page/Navigation"
+import Home from "./components/home";
 import { connect } from "react-redux";
 import "./style/app.scss";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { LOGIN_PAGE, REGISTER_PAGE,USER_PROFILE } from "./common/common";
+import { HOME_ROUTER ,LOGIN_PAGE, REGISTER_PAGE,USER_PROFILE } from "./common/common";
 
 class App extends Component {
   render() {
     return (
       <Router basename="/">
-        <section className="App">
-          <div className="app-bgn">
+        <div className="App">
+            <Navigation />
             <Switch>
+              <Route extra path={HOME_ROUTER} component={Home}></Route>
               <Route exact path={REGISTER_PAGE} component={Register} />
               <Route exact path={LOGIN_PAGE} component={Login} />
               <Route exact path={USER_PROFILE} component={Profile} />
             </Switch>
           </div>
-        </section>
       </Router>
     );
   }
