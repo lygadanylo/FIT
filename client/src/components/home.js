c7b7008da602a31aa4c6e1919c3dc5edb375da82
Lyga Danylo <dlyg@keplercode.com>
2019-07-08 00:43:54 -0700

    added styles

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,17 +1,23 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Home extends Component {
-    constructor(props){
-        super(props);
-        this.state={};
-    }
-    render(){
-        return(
-            <section is="home-wrapper">
-
-            </section>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <section id="home-wrapper">
+        <div className="title-wrapper col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+          <h1>Make you body</h1>
+          <h1>beautiful</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.
+          </p>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Home;
