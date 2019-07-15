d2cf1517130fa618de7543834b0ddbaf497dd232
lygadanylo <31656731+lygadanylo@users.noreply.github.com>
2019-07-15 22:25:17 +0300

    update in register/login method

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -8,18 +8,18 @@ export const login = (req, res, next) => {
     {
       email: email
     },
-    (error, user) => {
+    (error, existingUser) => {
       if (error) {
         return res
           .status(HttpStatus.BAD_REQUEST)
           .json({ success: false, message: false });
       }
-      if (!user) {
+      if (!existingUser) {
         return res
           .status(HttpStatus.BAD_REQUEST)
           .json({ type: false, message: "user not found" });
       }
-      const decryptedToken = CryptoJS.enc.Base64.parse(user.password);
+      const decryptedToken = CryptoJS.enc.Base64.parse(existingUser.password);
       const result = decryptedToken.toString(CryptoJS.enc.Utf8);
       const params = result.split(" ");
       const { password: userPassword } = req.body;
@@ -28,7 +28,7 @@ export const login = (req, res, next) => {
           .status(HttpStatus.BAD_REQUEST)
           .json({ type: false, message: "password or email is not valid" });
       }
-      const { email, name, lastName } = user;
+      const { email, name, lastName } = existingUser;
       return res.status(HttpStatus.OK).json({
         user: { email, name, lastName },
         message: "Welcome"
@@ -37,47 +37,28 @@ export const login = (req, res, next) => {
   ).lean();
 };
 
-export const register = (req, res, next) => {
+
+export const register = (req,res) => {
   const { email, name, last_name, password } = req.body;
-  const validationPassword = "/^[a-zA-Z0-9!@#$%^&*_=+-]{8,12}$/g";
-  const validationEmail =
-    "/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/";
-  if (email == "" || name == "" || last_name == "" || password == "") {
-    if (email != validationEmail || password != validationPassword) {
-      return res
-        .status(400)
-        .json({ type: false, message: "Email or Password invalid" });
+  User.findOne({email: email}, (error, existingUser) => {
+    if(error){
+      return res.status(HttpStatus.BAD_REQUEST).json({type:false, message: "Bad request"});
     }
-    return res.status(400).json({ success: false, message: "Field is empty" });
-  }
-  User.findOne(
-    {
-      email: email
-    },
-    (error, user) => {
-      console.log(email);
-      console.log(user.email);
-      if (error) {
-        return res
-          .status(400)
-          .json({ type: false, message: "This email is using now" });
-      }
-      if (email !== user.email) {
-        const token = CryptoJS.enc.Utf8.parse(`${password} ${email}`);
-        const passwordHash = CryptoJS.enc.Base64.stringify(token);
-        const Users = new User({
-          email,
-          name,
-          lastName: last_name,
-          password: passwordHash
-        }).save();
-        return res
-          .status(200)
-          .json({ type: true, user: Users, message: "Created" });
-      }
-      return res
-        .status(400)
-        .json({ type: false, message: "This email is using now" });
+    if(existingUser) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        message: "Account with that email address already exists."
+      });
     }
-  );
-};
+    const token = CryptoJS.enc.Utf8.parse(`${password} ${email}`);
+    const passwordHash = CryptoJS.enc.Base64.stringify(token);
+    const Users = new User({
+    email,
+    name,
+    lastName: last_name,
+    password: passwordHash
+    }).save();
+    return res
+    .status(200)
+    .json({ type: true, user: Users, message: "Created" });
+  })
+}
