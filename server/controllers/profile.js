539a5805b7b41103763e0ee955c9ecc3eef21244
Lyga Danylo <dlyg@keplercode.com>
2019-07-16 23:09:00 -0700

    fix server error

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,5 +1,5 @@
 import HttStatus from "http-status-codes";
 
-export const fetchUser = (req,res) => {
-    res.send(HttStatus.Ok).json({message:"Fetching User"});
-}
+export const fetchUser = (req, res) => {
+  return res.status(HttStatus.OK).json({ message: "Fetching User" });
+};
