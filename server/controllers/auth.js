add829844e4af7895307647135f87c1434c28c75
Lyga Danylo <dlyg@keplercode.com>
2019-07-19 03:45:18 -0700

    delete passport auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,22 +3,7 @@ import CryptoJS from "crypto-js";
 import HttpStatus from "http-status-codes";
 import passport from "passport";
 
-export const login = (req, res, next) => {
-  passport.authenticate("local", { session: false }, (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.status(HttpStatus.BAD_REQUEST).json(info);
-    }
-    req.logIn(user, err => {
-      if (err) {
-        return next(err);
-      }
-      return next();
-    });
-  })(req, res, next);
-};
+export const login = (req, res, next) => {};
 
 export const register = (req, res) => {
   const { email, name, last_name, password } = req.body;
@@ -45,14 +30,4 @@ export const register = (req, res) => {
       .status(200)
       .json({ type: true, user: Users, message: "Created" });
   });
-};
-
-export const onAuth = (req, res) => {
-  if (!req.user) {
-    return res
-      .status(HttpStatus.UNAUTHORIZED)
-      .json({ msg: "User Not Authenticated" });
-  }
-  const user = req.user;
-  return res.status(HttpStatus.OK).json({ msg: "Success", user });
 };
--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ import apiRouts from "./routs/api";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import passport from "./config/passport";
-import session from "express-session";
 
 const app = express();
 const PORT = 3001;
@@ -11,21 +9,6 @@ const PORT = 3001;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(
-  session({
-    secret: "SECRET",
-    cookie: {
-      path: "/",
-      httpOnly: true,
-      maxAge: 60 * 60 * 1000
-    },
-    resave: false,
-    saveUninitialized: false
-  })
-);
 
 app.use("/api", apiRouts);
 
